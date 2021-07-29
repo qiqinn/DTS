9571199ed6fbb196563b499118444dc9515ca8a0
qiqinn <qin@webmail.umm.ac.id>
2021-07-29 15:18:13 +0800

    update sesi 8

--- Tugas Sesi 8 - Muthaqin Dean.py
+++ Tugas Sesi 8 - Muthaqin Dean.py
@@ -1,5 +1,30 @@
-x = input()
-y = input()
+arr = []
+n = x = int(input("Banyaknya element : "))
+for i in range(0,n):
+    x = int(input("Nilai element ke - "+ str(i+1)+ " : "))
+    arr.append(x)
 
 
-print(x+y)
+#fungsi menghitung rata-rata(mean)
+def mean(l):
+  sum = 0
+  for ele in l:
+    sum = sum + ele
+  return sum / n
+
+rataRata = mean(arr)
+
+#tiap elemen data dikurang rata - rata dan dikuadratkan
+res = [(ele - rataRata) ** 2 for ele in arr]
+
+#fungsi menghitung semua elemen list
+def sumList(l):
+  sum = 0
+  for ele in l:
+    sum = sum + ele
+  return sum
+
+sigma = sumList(res)
+
+stdDeviasi = (sigma / n) ** 0.5
+print ("Standar Deviasi :", stdDeviasi)
